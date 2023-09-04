b0d05dd464eed2ce89865af78222332eae40413b
zoe <joheun2390@gmail.com>
2023-09-04 18:08:40 +0900

    20230904

---
+++ 03.구현소스/js/mdla.js
@@ -0,0 +1,24 @@
+// MDLA js - mdla.js
+
+const qs = x => document.querySelector(x);
+const qsa = x => document.querySelectorAll(x);
+
+window.addEventListener('DOMContentLoaded',loadFn);
+
+// 로드함수
+function loadFn(){
+    // 함수호출확인
+    console.log('로딩!');
+
+    // 시나리오
+    // nav-item에 들어갈 텍스트들을 배열로 나열한 후에,
+    // span으로 단어를 하나씩 감싼뒤에, transition을 0.1씩 느리게 준다.
+
+    // 1. 대상: .nav-item
+    // 1-1. 대상확인
+    let nav_item = qsa('')
+
+    
+} //////// loadFn //////////
+//////////////////////////////
+const nav_item = qs('.nav-item');
