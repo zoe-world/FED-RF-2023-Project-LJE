6a75408ff3beea6ca383d4a150ee2f563799243f
zoe <joheun2390@gmail.com>
2023-09-05 18:18:58 +0900

    20230905

--- 03.구현소스/js/mdla.js
+++ 03.구현소스/js/mdla.js
@@ -1,16 +1,16 @@
 // MDLA js - mdla.js
 
 // DOM 선택함수
-const qs = x => document.querySelector(x);
-const qsa = x => document.querySelectorAll(x);
+const qs = (x) => document.querySelector(x);
+const qsa = (x) => document.querySelectorAll(x);
 
 // 로드구역 //////////////
-window.addEventListener('DOMContentLoaded',loadFn);
+window.addEventListener("DOMContentLoaded", loadFn);
 
 // 로드함수 /////////////
-function loadFn(){
+function loadFn() {
     // 함수호출확인
-    console.log('로딩!');
+    console.log("로딩!");
 
     /* 
         시나리오
@@ -19,44 +19,34 @@ function loadFn(){
         .nav-item 배열 개수에 맞게 생성해서 그 안에 집어 넣어야함
         span으로 단어를 하나씩 감싼뒤에, transition을 0.1씩 느리게 준다. 
     */
-   // 0. 네비에 들어가야하는 텍스트
-        const nav_name = [
-            "Resort 2024",
-            "Top stories",
-            "Trend reports",
-            "Latest shows"
-        ];
-
-    // console.log('네비확인:',nav_name[0],Array.isArray(nav_name));
+    // 0. 네비에 들어가야하는 텍스트
+    const navName = ["Resort 2024", "Top stories", "Trend reports", "Latest shows"];
 
     // 1. 대상: .nav-item
     // 1-1. 대상확인
-    const nav_li = qsa('.nav-li');    
-    const nav_item = qsa('.nav-item');    
-    let nav_txt = nav_item.innerText;
-    let new_nav = '';
+    const navLink = qsa(".nav-item");
 
     // 2. 이벤트 대상
-    
-    // for (let x of nav_name){
-    //     console.log('x:',x)
-    //     new_nav += `<span>${x}</span>`;
-    // }
-
+    //.nav-item .btn-text 안에 span
     const newEle = (txt) => {
-        
-    for (let x of txt){
-        // console.log('x:',x)
-        new_nav += `
-        <li>
-            <a href="#" class="nav-item"><span>${x}</span></a>
-        </li>
-        `;
-    }
-    return new_nav;
-        
-    }
+        let new_nav = "";
+        for (let x of txt) {
+            new_nav += `<span>${x}</span>`;
+        }
+        return new_nav;
+    };
+    navLink.forEach((ele, idx) => {
+        (ele.innerHTML = `
+            <span class="btn-text">${newEle(navName[idx])}</span>
+            <span class="btn-text2">${newEle(navName[idx])}</span>
+        `)
+    });
 
-    qsa('.nav-list li').forEach((ele,idx)=>ele.innerHTML=`${newEle(nav_name[idx])}`)
+    // 3. 생성된 span에 transition주기
+    let navTxt = qsa('.btn-text span');
+    navTxt.forEach((ele,idx)=>{
+        (ele.style.transitionDelay = (0.05*idx) + 's')
+    })
 
+    console.log(navTxt);
 } //////// loadFn //////////
