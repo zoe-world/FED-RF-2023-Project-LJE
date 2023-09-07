aa54f45e669945440ba1a2e2637b5c3fcdbb0727
zoe <joheun2390@gmail.com>
2023-09-07 16:29:02 +0900

    20230907

--- 03.구현소스/js/mdla.js
+++ 03.구현소스/js/mdla.js
@@ -17,17 +17,19 @@ function loadFn() {
         nav-item에 들어갈 텍스트들을 배열로 나열한 후에,
         그 텍스트들을 .nav-item에 넣어줘야함
         .nav-item 배열 개수에 맞게 생성해서 그 안에 집어 넣어야함
-        span으로 단어를 하나씩 감싼뒤에, transition을 0.1씩 느리게 준다. 
+        span으로 단어를 하나씩 감싼뒤에, 단어 별로 끊어서 transition을 0.1씩 느리게 준다. 
     */
     // 0. 네비에 들어가야하는 텍스트
     const navName = ["Resort 2024", "Top stories", "Trend reports", "Latest shows"];
-
+    
     // 1. 대상: .nav-item
     // 1-1. 대상확인
     const navLink = qsa(".nav-item");
-
+    let navEach =[];
+    let navTxt = '';
     // 2. 이벤트 대상
     //.nav-item .btn-text 안에 span
+
     const newEle = (txt) => {
         let new_nav = "";
         for (let x of txt) {
@@ -35,18 +37,25 @@ function loadFn() {
         }
         return new_nav;
     };
+
     navLink.forEach((ele, idx) => {
-        (ele.innerHTML = `
+        navEach.push(newEle(navName[idx]));
+        ele.innerHTML = `
             <span class="btn-text">${newEle(navName[idx])}</span>
             <span class="btn-text2">${newEle(navName[idx])}</span>
-        `)
+        `;
     });
 
-    // 3. 생성된 span에 transition주기
-    let navTxt = qsa('.btn-text span');
-    navTxt.forEach((ele,idx)=>{
-        (ele.style.transitionDelay = (0.05*idx) + 's')
-    })
-
+    // 네비게이션 글자를 받아옴
+    navTxt = navEach.toString();
     console.log(navTxt);
+
+    // 3. 생성된 span에 transition주기
+    navTxt.forEach((ele,idx)=>{
+        // console.log(ele,idx,array);
+        ele[idx].querySelectorAll('span').style.transitionDelay =  (0.05*idx) +'s';
+    });
+
+
+    
 } //////// loadFn //////////
