7dd51c62e45dda0f04044295d0b5aa387026a81f
zoe <joheun2390@gmail.com>
2023-09-20 19:28:05 +0900

    sticky-menu 클릭시 스크롤 튕김

--- 03.구현소스/js/mdla.js
+++ 03.구현소스/js/mdla.js
@@ -22,6 +22,24 @@ const domFn = {
 /* 부드러운 스크롤 호출 */
 startSS();
 
+// 0. 새로고치면 스크롤바 위치캐싱후 맨위로 이동
+setTimeout(() => {
+  // 윈도우 스크롤 맨위로!
+  window.scrollTo(0, 0);
+  // 부드러운 스크롤 위치값 반영!
+  setPos(0);
+  // 안하면 원래 위치로 스크롤시 튐!
+}, 400);
+// 0. 스크롤바 트랙을 잡고 위치이동시 위치값 반영
+domFn.addEvt(window, "mouseup", () => setPos(window.scrollY));
+//////// mouseup /////////////
+const atag = domFn.qsa(".sticky-menu a");
+console.log(atag)
+// 0. 키보드 방향키 이동시 위치값 반영
+domFn.addEvt(window, "keyup", () => setPos(window.scrollY));
+domFn.addEvt(atag, "click", () => setPos(window.scrollY));
+//////// keyup /////////////
+
 // nav-item 에 들어가는 span html 로 넣기
 
 const navName = [
