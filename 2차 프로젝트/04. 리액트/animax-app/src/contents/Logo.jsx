166c3d5b1fae4300b7402f6a9d146fd1fed52d28
zoe <joheun2390@gmail.com>
2023-11-09 18:40:51 +0900

    20231109

---
+++ 2차 프로젝트/04. 리액트/animax-app/src/contents/Logo.jsx
@@ -0,0 +1,12 @@
+// Animax logo 컴포넌트
+import React from "react";
+
+export const Logo = () => {
+    return(
+        <h1 className="logo">
+        <a href="#">
+            <img src="./images/logo.png" alt="Animax" />
+        </a>
+        </h1>
+    );
+}
---
+++ 2차 프로젝트/04. 리액트/animax-app/src/layout/MainArea.jsx
@@ -0,0 +1,8 @@
+// 메인영역 컴포넌트
+export function MainArea(){
+    return(
+        <>
+            
+        </>
+    );
+} ////// MainArea 컴포넌트 //////
