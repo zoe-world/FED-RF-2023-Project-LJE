827dab5309467cbf9c4fc964ebd1371420ff1a85
zoe <joheun2390@gmail.com>
2023-11-16 18:56:14 +0900

    20231116

--- 2차 프로젝트/04. 리액트/animax-app/src/components/layout/FooterArea.jsx
+++ 2차 프로젝트/04. 리액트/animax-app/src/components/layout/FooterArea.jsx
@@ -1,9 +1,26 @@
 // Animax 하단영역 컴포넌트
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faXTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export function FooterArea(){
     return(
         <>
-            
+            <div id="footer_area">
+                <footer className="footer_area">
+                    <div className="info_box">
+                        <dl className="ntc_list">
+                            <dt className="tag">공지사항</dt>
+                            <dd className="tit"><a href="#">샹그릴라 프론티어 -망겜 헌터 갓겜에 도전하다- 방송시간 변경 안내</a></dd>
+                        </dl>
+                        <div className="sns_box">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                            <FontAwesomeIcon icon={faXTwitter} />
+                            <FontAwesomeIcon icon={faYoutube} />
+                        </div>
+                    </div>
+                </footer>
+            </div>
         </>
     );
 }////// FooterArea 컴포넌트 ////
